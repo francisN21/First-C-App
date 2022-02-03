f589532cd9902d63764c078c8c91103797ae88ea
Francisco Rones <ininorones@gmail.com>
2022-02-02 23:52:28 -0800

    First commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    printf("Enter Age:\n");
+    scanf("insert number only: %d", &age);
+    printf("Age is %d", age);
+    cout << "\nHello World!";
+    cout << "\n";
+
+    return 0;
+}
